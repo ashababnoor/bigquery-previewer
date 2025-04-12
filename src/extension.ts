5e2ccc9f3a0b21bd0b361634ffe1173df995315a
Ahmed Shabab Noor <57612672+ashababnoor@users.noreply.github.com>
2025-04-12 11:03:14 +0600

    refactor: remove unused helloWorld command from activation

--- src/extension.ts
+++ src/extension.ts
@@ -68,13 +68,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bigquery-previewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from BigQuery Previewer!');
-	});
-
-	context.subscriptions.push(disposable);
 
 	// Register the command for manual analysis
 	const analyzeQueryCommand = vscode.commands.registerCommand('bigquery-previewer.analyzeQuery', async () => {
